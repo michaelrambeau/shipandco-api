1e9e0bbaa265bac5918e617c21f566230d21f176
michaelrambeau <mikeair@gmail.com>
2016-11-11 07:32:43 +0900

    Auth process (WIP)

--- src/services/index.js
+++ src/services/index.js
@@ -21,8 +21,30 @@ function startServices (app) {
     console.log(req.cookies);
     const token = req.cookies['feathers-jwt']
     console.log('token=', token);
-    res.cookie('feathers-jwt', token)
+    res.cookie('feathers-jwt', token, {
+      maxAge: 1000 * 10
+    })
     res.redirect('http://localhost:3000/')
+  })
+
+  app.get('/login/request', (req, res) => {
+    const APP_URL = 'https://shipandco.auth0.com'
+    const client_id = '3w7EXmzzpZ4dkQGQuO6yhYAIRbLrQVIb'
+    // const client_id = 'DUuPWkiKhQexHjZbpVO7b9wKMj2f7tsq'
+    const source = req.query.source
+    const redirect_uri = `/login/success?source=${source}`
+    // const auth0Client = 'eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiNi44LjAifQ'
+    const url = `${APP_URL}/authorize?scope=openid&response_type=token&connection=google-oauth2&sso=true&client_id=${client_id}&redirect_uri=${redirect_uri}`
+    res.redirect(url)
+  })
+  app.get('/login/callback', (req, res) => {
+    const token = req.cookies['feathers-jwt']
+    console.log('>>> token=', token);
+    res.cookie('feathers-jwt', token, {
+      maxAge: 1000 * 10
+    })
+    const url = req.query.source
+    res.redirect(url)
   })
 
   app.use('/customers', customersService)
