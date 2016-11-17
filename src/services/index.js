63a26657e5cd06a81106e5a386c8766968bf2804
michaelrambeau <mikeair@gmail.com>
2016-11-18 07:22:59 +0900

    Update auth process

--- src/services/index.js
+++ src/services/index.js
@@ -6,6 +6,7 @@ const shipmentsService = require('./shipments')
 const shopsService = require('./shops')
 const dashboardService = require('./dashboard')
 const auth = require('feathers-authentication').hooks
+const path = require('path')
 
 const userEndPoint = '/admin-users'
 const WEB_CLIENT_COOKIE = 'web-client-url'
@@ -14,8 +15,13 @@ function startServices (app) {
   // Add a middleware to write in a cookie where the user comes from
   // This cookie will be user later to redirect the user to the single-page application.
   app.get('/auth/auth0', (req, res, next) => {
-    const { source } = req.query
-    res.cookie(WEB_CLIENT_COOKIE, source)
+    const { source, domain } = req.query
+    if (source) {
+      res.cookie(WEB_CLIENT_COOKIE, source)
+      res.cookie('domain', domain)
+    } else {
+      res.clearCookie(WEB_CLIENT_COOKIE)
+    }
     next()
   })
   // setup Auth0 service that will automatically create `/auth/auth0` route
@@ -26,12 +32,21 @@ function startServices (app) {
   // Route called after a successful login
   // Redirect the user to the single-page application "forwarding" the auth token
   app.get('/auth/success', function (req, res) {
-    const token = req.cookies['feathers-jwt']
-    res.cookie('feathers-jwt', token, {
-      maxAge: 1000 * 10 // short-term cookie (10 seconds)
-    })
     const url = req.cookies[WEB_CLIENT_COOKIE]
-    res.redirect(url)
+    const domain = req.cookies['domain']
+    if (url) {
+      //  if there is a cookie that contains the URL source, redirect the user to this URL,
+      // "forwarding" the short-term cookie that contains the user's token.
+      const token = req.cookies['feathers-jwt']
+      res.cookie('feathers-jwt', token, {
+        maxAge: 1000 * 10, // will expire in 10 seconds
+        domain
+      })
+      res.redirect(url)
+    } else {
+      // otherwise send the static page on the same domain.
+      res.sendFile(path.resolve(process.cwd(), 'public', 'success.html'))
+    }
   })
 
   // Register all REST services
