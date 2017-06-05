ce700c8c484a04e3ec0b401fab327536d7732876
michaelrambeau <mikeair@gmail.com>
2017-06-05 16:02:40 +0900

    Improve shipment by userId performance

--- src/services/shipments/index.js
+++ src/services/shipments/index.js
@@ -2,6 +2,36 @@ const MongooseService = require('feathers-mongoose').Service
 const Model = require('./Shipment')
 const fields = require('./fields')
 
+// Opitimized search by user using `hint` to perform faster `count()` operations
+function findByUser (query) {
+  const { userId, $skip, $limit } = query
+  const limit = parseInt($limit)
+  const skip = parseInt($skip)
+  const fieldHashMap = fields.reduce(
+    (acc, field) => Object.assign({}, acc, {
+      [field]: 1
+    }),
+    {}
+  )
+  const getShipments = Model
+    .find({ userId })
+    .sort('-date')
+    .skip(skip)
+    .limit(limit)
+    .select(fieldHashMap)
+  const getTotal = Model
+    .count({ userId })
+    .hint({ userId: 1, date: -1 })
+  return Promise.all([getShipments, getTotal])
+    .then(([shipments, total]) => ({
+      data: shipments,
+      limit,
+      skip,
+      total,
+      custom: true
+    }))
+}
+
 class ShipmentsService extends MongooseService {
   find (params) {
     const $select = fields
@@ -12,22 +42,10 @@ class ShipmentsService extends MongooseService {
     const updatedParams = Object.assign({}, params, {
       query
     })
-    const fieldHashMap = fields.reduce(
-        (acc, field) => Object.assign({}, acc, {
-          [field]: 1
-        }),
-        {}
-      )
-    console.log('Fields', fieldHashMap)
-    // return Model
-      // .count({ userId: params.query.userId })
+    if (params.query.userId) {
+      return findByUser(query)
+    }
     return super.find(updatedParams)
-    // return Model
-    //   .find({ userId: params.query.userId })
-    //   .sort(params.query.$sort)
-    //   .skip(parseInt(params.query.$skip))
-    //   .limit(parseInt(params.query.$limit))
-    //   .select(fieldHashMap)
   }
   get (id, params) {
     return Model.findOne({_id: id}).select({ 'shipment_infos.label': 0 })
